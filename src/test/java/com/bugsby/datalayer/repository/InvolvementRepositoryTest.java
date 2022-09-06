52fc8c1fc67349d95579c4597a23f71c081ccef4
Oskar Picus <oskarpicus@yahoo.com>
2022-09-06 13:18:41 +0300

    Fixed tests

---
+++ src/test/java/com/bugsby/datalayer/repository/InvolvementRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.bugsby.datalayer.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class InvolvementRepositoryTest {
+    @Autowired
+    private InvolvementRepository involvementRepository;
+
+    @Test
+    void verifyBeanIsCorrectlyConstructed() {
+
+    }
+}
---
+++ src/test/java/com/bugsby/datalayer/repository/IssueRepositoryTest.java
@@ -0,0 +1,19 @@
+package com.bugsby.datalayer.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class IssueRepositoryTest {
+    @Autowired
+    private IssueRepository issueRepository;
+
+    @Test
+    void verifyBeanIsCorrectlyConstructed() {
+
+    }
+}
