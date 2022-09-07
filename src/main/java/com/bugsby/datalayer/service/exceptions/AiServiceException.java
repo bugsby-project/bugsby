fed7afc333f1716cd8dbd1d3e205357b6f5c7ad3
oskarpicus <57806887+oskarpicus@users.noreply.github.com>
2022-09-07 11:54:18 +0300

    Task/#12 (#22)
    * Refactored exceptions to be unchecked
    
    * Refactored MasterService

--- src/main/java/com/bugsby/datalayer/service/exceptions/AiServiceException.java
+++ src/main/java/com/bugsby/datalayer/service/exceptions/AiServiceException.java
@@ -1,6 +1,6 @@
 package com.bugsby.datalayer.service.exceptions;
 
-public class AiServiceException extends Exception {
+public class AiServiceException extends RuntimeException {
     public AiServiceException(String message) {
         super(message);
     }
--- src/main/java/com/bugsby/datalayer/service/exceptions/UserNotFoundException.java
+++ src/main/java/com/bugsby/datalayer/service/exceptions/UserNotFoundException.java
@@ -1,6 +1,6 @@
 package com.bugsby.datalayer.service.exceptions;
 
-public class UserNotFoundException extends Exception {
+public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
     }
