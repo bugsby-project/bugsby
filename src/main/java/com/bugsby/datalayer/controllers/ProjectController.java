fa2abe720082a8c9aeab800e82f3bf0798f203af
Oskar Picus <oskarpicus@yahoo.com>
2022-12-19 13:50:39 +0200

    Added dedicated endpoints to retrieve issues list & involvements list of a project

--- src/main/java/com/bugsby/datalayer/controllers/ProjectController.java
+++ src/main/java/com/bugsby/datalayer/controllers/ProjectController.java
@@ -1,9 +1,15 @@
 package com.bugsby.datalayer.controllers;
 
+import com.bugsby.datalayer.model.Involvement;
+import com.bugsby.datalayer.model.Issue;
 import com.bugsby.datalayer.model.Project;
 import com.bugsby.datalayer.service.Service;
 import com.bugsby.datalayer.service.exceptions.ProjectNotFoundException;
 import com.bugsby.datalayer.swagger.api.ProjectsApi;
+import com.bugsby.datalayer.swagger.model.InvolvementResponse;
+import com.bugsby.datalayer.swagger.model.InvolvementsList;
+import com.bugsby.datalayer.swagger.model.IssueList;
+import com.bugsby.datalayer.swagger.model.IssueResponse;
 import com.bugsby.datalayer.swagger.model.ProjectRequest;
 import com.bugsby.datalayer.swagger.model.ProjectResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +30,12 @@ public class ProjectController implements ProjectsApi {
     private Function<Project, ProjectResponse> projectResponseMapper;
     @Autowired
     private Function<ProjectRequest, Project> projectRequestMapper;
+    @Autowired
+    private Function<Involvement, InvolvementResponse> involvementResponseMapper;
+    @Autowired
+    private Function<Issue, IssueResponse> issueResponseMapper;
+
+    private static final String PROJECT_DOES_NOT_EXIST_MESSAGE = "Project does not exist";
 
     @Override
     public ResponseEntity<ProjectResponse> createProject(String authorization, ProjectRequest body) {
@@ -34,10 +46,32 @@ public class ProjectController implements ProjectsApi {
     }
 
     @Override
+    public ResponseEntity<InvolvementsList> getInvolvementsByProjectId(String authorization, Long id) {
+        return Optional.ofNullable(service.getProjectById(id))
+                .map(project -> project.getInvolvements().stream()
+                        .map(involvementResponseMapper)
+                        .toList())
+                .map(involvementResponses -> new InvolvementsList().involvements(involvementResponses))
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ProjectNotFoundException(PROJECT_DOES_NOT_EXIST_MESSAGE));
+    }
+
+    @Override
+    public ResponseEntity<IssueList> getIssuesByProjectId(String authorization, Long id) {
+        return Optional.ofNullable(service.getProjectById(id))
+                .map(project -> project.getIssues().stream()
+                        .map(issueResponseMapper)
+                        .toList())
+                .map(issues -> new IssueList().issues(issues))
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ProjectNotFoundException(PROJECT_DOES_NOT_EXIST_MESSAGE));
+    }
+
+    @Override
     public ResponseEntity<ProjectResponse> getProjectById(String authorization, Long id) {
         return Optional.ofNullable(service.getProjectById(id))
                 .map(projectResponseMapper)
                 .map(ResponseEntity::ok)
-                .orElseThrow(() -> new ProjectNotFoundException("Project does not exist"));
+                .orElseThrow(() -> new ProjectNotFoundException(PROJECT_DOES_NOT_EXIST_MESSAGE));
     }
 }
